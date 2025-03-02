23a3feeebe103ff566e7e7aca4e5a4b0d46141c3
Lane <l_dewald3@mail.fhsu.edu>
2025-03-02 16:36:56 -0600

    Added Main Python File with intial comments and imports that are likely to be used with project

---
+++ Main.py
@@ -0,0 +1,10 @@
+### INF601 - Advanced Programming in Python
+### Lane Dewald
+### Mini Project 1
+
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from faker import Faker
+import random
